c35aa51be4079669f2d27532400727fe1e44fc20
Fahad Anwar <officerops5@gmail.com>
2024-09-23 16:22:44 +0500

    ConditionalValues.c
    In this program i verified some values whether they are correct or wrong, on the base of given conditions.
    
    if q>r
    and s>p
    if sum of r and s > than the sum of p and q.

---
+++ ConditionalValues.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+
+int p,q,r,s;
+printf("Enter second integer\n");
+scanf("%d", &q);
+printf("Enter third integer\n");
+scanf("%d", &r);
+printf("Enter fourth integer\n");
+scanf("%d", &s);
+
+if (q>r,s>p)
+{
+    if (r+s>p+q)
+    {
+       printf("Correct Values");
+    }
+}
+else
+{
+    printf("Wrong Values");
+}
+
+return 0;
+}
